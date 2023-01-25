992b09b78be11818025c4d95b42897ab7a93ab1a
barboza1719 <71381120@certus.edu.pe>
2023-01-25 09:15:32 -0500

    Version Inicial
    Version Inicial

---
+++ AplicacionErka/src/main/java/pe/com/erka/AplicacionerkaApplication.java
@@ -0,0 +1,13 @@
+package pe.com.erka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionerkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionerkaApplication.class, args);
+	}
+
+}
